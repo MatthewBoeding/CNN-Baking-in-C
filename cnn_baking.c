766f4a3511c2ed54f6e7a8674deb5d29c85870c6
MatthewBoeding <mboeding@huskers.unl.edu>
2022-08-25 13:25:25 -0500

    Initial Linear Algebra Attempt

---
+++ cnn_baking.c
@@ -0,0 +1,22 @@
+
+
+
+int main()
+{
+    while(1)
+    {
+        for(int i = 0; i < 10000; i++)
+        {
+            for(int i = 0; i <30000; i++)
+            {
+
+            }
+        }
+        printf("debug success");
+        for(int i = 0; i <30000; i++)
+        {
+
+        }
+    }
+
+}
