3970da3791cc3a126c5507fe5330f99426dfda3b
MatthewBoeding <mboeding@huskers.unl.edu>
2022-08-30 11:28:57 -0500

    Convolution start!

--- linearalgebra.c
+++ linearalgebra.c
@@ -62,7 +62,13 @@ void matrix_transpose(struct matrix * mat)
         val += (rows*cols);
     }   
 }
-
+/**
+ * @brief returs dot product of any 2 matrices (assuming mat1 cols == mat2 rows)
+ *  3D matrices are also supported
+ * @param mat1 - first matrix in multiplication (depth x rows x height) 
+ * @param mat2 - second matrix of form depth x rows x height
+ * @param result - resulting matrix of size depth, mat1->rows, mat2->columns
+ */
 void matrix_dotproduct(struct matrix * mat1, struct matrix * mat2, struct matrix * result)
 {
     *result->rows = *mat1->rows;
@@ -96,6 +102,11 @@ void matrix_dotproduct(struct matrix * mat1, struct matrix * mat2, struct matrix
     }
 }
 
+/**
+ * @brief frees all memory malloc'd in matrix_create
+ * Be sure to call this when you're done with a matrix!
+ * @param mat - matrix to have memory de-allocated
+ */
 void matrix_free(struct matrix * mat)
 {
     double * ptr_val = *mat->value;
@@ -107,10 +118,40 @@ void matrix_free(struct matrix * mat)
     return;
 
 }
-void matrix_convolution(struct matrix * mat1, struct matrix * mat2, struct matrix * result)
+/**
+ * @brief performs convolution (or cross-correlation) on 2 matrices
+ *  assume that all matrices have been malloc'd and stride = 1
+ * @param input - must be larget than kernel with same depth
+ * @param kernel - must be smaller than input with same depth
+ * @param output - 
+ */
+void matrix_convolution(struct matrix * input, struct matrix * kernel, struct matrix * output)
 {
-
-
+    if(*kernel->rows > *input->rows || *kernel->columns > *input->columns || *input->depth != *kernel->depth)
+    {
+     printf("Kernel is too large for input matrix");   
+    }
+    else
+    {
+        double value = 0;
+        int iterations = *output->rows+*output->columns;
+        int iterations_per_row = *input->columns - *kernel->columns + 1;
+        int row_offset, col_offset;
+        for(int i = 0; i < iterations; i++)
+        {
+            row_offset = iterations / iterations_per_row;
+            col_offset = iterations % iterations_per_row;
+            value = 0;
+            for(int r = 0; r < kernel->rows; r++)
+            {
+                for(int c = 0; c < kernel->columns; c++)
+                {
+                    value += *kernel->value[r*(*kernel->columns)+c] * *input->value[(r+row_offset)*(*kernel->columns)+c+col_offset];
+                }
+            }
+            *output->value[i]=value;
+        }
+    }
 }
 
 void matrix_addition(struct matrix * mat1, struct matrix * mat2, struct matrix * result)
